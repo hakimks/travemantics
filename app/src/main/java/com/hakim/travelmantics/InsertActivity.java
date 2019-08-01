96d74a6aef1a7dfa4aca4df0daa9b498ac27a77a
hakimks <hakimks@gmail.com>
2019-08-01 16:26:41 +0300

    Added a save item menu

--- app/src/main/java/com/hakim/travelmantics/InsertActivity.java
+++ app/src/main/java/com/hakim/travelmantics/InsertActivity.java
@@ -2,6 +2,8 @@ package com.hakim.travelmantics;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.insert_acivity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
