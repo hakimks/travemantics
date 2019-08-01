796a60c46e532d42825305f47ea32bddb13c0ac2
hakimks <hakimks@gmail.com>
2019-08-01 15:17:59 +0300

    Set up firebase to the project and migrate it to androidX

--- app/src/main/java/com/hakim/travelmantics/InsertActivity.java
+++ app/src/main/java/com/hakim/travelmantics/InsertActivity.java
@@ -1,6 +1,6 @@
 package com.hakim.travelmantics;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class InsertActivity extends AppCompatActivity {
