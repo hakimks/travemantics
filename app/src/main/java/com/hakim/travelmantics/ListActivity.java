f102d94b851c3924f84103be38ca0375fae96548
hakimks <hakimks@gmail.com>
2019-08-02 22:01:43 +0300

    Refactor: Rename the class

--- app/src/main/java/com/hakim/travelmantics/ListActivity.java
+++ app/src/main/java/com/hakim/travelmantics/ListActivity.java
@@ -1,7 +1,5 @@
 package com.hakim.travelmantics;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,11 +9,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -56,7 +51,7 @@ public class ListActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.insert_menu:
-                Intent intent = new Intent(this, InsertActivity.class);
+                Intent intent = new Intent(this, DealActivity.class);
                 startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
