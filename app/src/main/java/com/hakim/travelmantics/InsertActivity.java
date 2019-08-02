e0a6a4d495e9204f4ff11d463bbce3c016d4c555
hakimks <hakimks@gmail.com>
2019-08-02 19:46:55 +0300

    Feat: Added a FirebaseUtil class to put all code which deal with the firebase directly

--- app/src/main/java/com/hakim/travelmantics/InsertActivity.java
+++ app/src/main/java/com/hakim/travelmantics/InsertActivity.java
@@ -23,8 +23,8 @@ public class InsertActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.insert_acivity);
 
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
 
         txtTitle = (EditText) findViewById(R.id.txtTitle);
         txtPrice = (EditText) findViewById(R.id.txtPrice);
